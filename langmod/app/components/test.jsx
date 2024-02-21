54660efedce795c684b86d50bfe20d8afe65b1eb
Girendra Singh <126884309+Giru-03@users.noreply.github.com>
2024-02-21 22:30:52 +0530

    Create test.jsx

---
+++ langmod/app/components/test.jsx
@@ -0,0 +1,12 @@
+<main>
+        <div class="guide">
+            <button class="btn">START</button>
+            <button class="btn">CONVERT</button>
+        </div>
+        <div class="cam">
+
+        </div>
+        <div class="result">
+            <button class="btn">LISTEN</button>
+        </div>
+    </main>
